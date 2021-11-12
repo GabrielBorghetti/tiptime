4fa6872ba82d77a81a96e0557ce6c2085b909a41
Gabriel Borghetti Miranda <gabriel.miranda@ecocentauro.com.br>
2021-11-12 17:59:26 -0300

    feat new func

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -25,31 +25,30 @@ class MainActivity : AppCompatActivity() {
             val roundDown = binding.roundDownSwitch.isChecked
             if (stringInTextField.isEmpty()) {
                 val builder = androidx.appcompat.app.AlertDialog.Builder(this)
-                builder.setMessage("Cost of Service is needed")
-                builder.setPositiveButton("ok, understood!",
-                    { dialogInterface: DialogInterface?, i: Int -> return@setPositiveButton })
-                builder.show()
+                    builder.setMessage("Cost of Service is needed")
+                        builder.setPositiveButton("ok, understood!",
+                            { dialogInterface: DialogInterface?, i: Int -> return@setPositiveButton })
+                                 builder.show()
             } else if (roundDown && roundUp) {
                 val builder = androidx.appcompat.app.AlertDialog.Builder(this)
-                builder.setMessage("You can only choose one option to round it")
-                builder.setPositiveButton("OK, understood!",
-                    { dialogInterface: DialogInterface?, i: Int -> return@setPositiveButton })
-                builder.show()
+                    builder.setMessage("You can only choose one option to round it")
+                        builder.setPositiveButton("OK, understood!",
+                                { dialogInterface: DialogInterface?, i: Int -> return@setPositiveButton })
+                                    builder.show()
             } else {
                 calculateTip()
             }
         }
         binding.btAbout.setOnClickListener {
             val intent = Intent(applicationContext, About::class.java)
-                startActivity(intent)
+            startActivity(intent)
         }
         binding.SavedTipsButton.setOnClickListener {
             val intent = Intent(applicationContext, SaveTips::class.java)
-                startActivity(intent)
+            startActivity(intent)
         }
-        binding.SaveTipButton.setOnClickListener{
-            val intent = Intent(applicationContext, SaveTips::class.java)
-                startActivity(intent)
+        binding.SaveTipButton.setOnClickListener {
+            savetip()
         }
 
     }
@@ -63,10 +62,10 @@ class MainActivity : AppCompatActivity() {
         val cost = stringInTextField.toDouble()
         val selectedId = binding.tipOptions.checkedRadioButtonId
         val tipPercentage = when (selectedId) {
-            R.id.option_twenty_percent -> 0.20
-            R.id.option_eighteen_percent -> 0.18
-            R.id.option_fifteen_percent -> 0.15
-            else -> 0.05
+                R.id.option_twenty_percent -> 0.20
+                    R.id.option_eighteen_percent -> 0.18
+                        R.id.option_fifteen_percent -> 0.15
+                            else -> 0.05
         }
         var tip = tipPercentage * cost
         val roundUp = binding.roundUpSwitch.isChecked
@@ -84,13 +83,31 @@ class MainActivity : AppCompatActivity() {
         val finalPrice = tip + cost
         val costCurrency = NumberFormat.getCurrencyInstance().format(cost)
         val finalPriceCurrency = NumberFormat.getCurrencyInstance().format(finalPrice)
-        binding.costOfServiceText.text = getString(R.string.cost_of_service_text, costCurrency)
-        binding.tipResult.text = getString(R.string.tip_amount_number, formattedTip)
-        binding.finalPrice.text = getString(R.string.finalPrice, finalPriceCurrency)
+            binding.costOfServiceText.text = getString(R.string.cost_of_service_text, costCurrency)
+                binding.tipResult.text = getString(R.string.tip_amount_number, formattedTip)
+                    binding.finalPrice.text = getString(R.string.finalPrice, finalPriceCurrency)
+        var teste = formattedTip
+    }
 
+    private fun savetip() {
 
+        if(binding.tipResult.text.toString() == ("Tip Amount: - - - - ")) {
+            val builder = androidx.appcompat.app.AlertDialog.Builder(this)
+                    builder.setTitle("Error!")
+                        builder.setMessage("You must calculate a tip before saving it")
+                            builder.setPositiveButton(
+                "ok, understood!",
+                { dialogInterface: DialogInterface?, i: Int -> return@setPositiveButton })
+            builder.show()
+        }else{
+                val builder = androidx.appcompat.app.AlertDialog.Builder(this)
+                        builder.setTitle("Tip Saved!")
+                            builder.setMessage("You've saved your tip! Check on 'Saved Tips' to see all of your tips")
+                                builder.show()
+        }
     }
 }
+
 
 
 
